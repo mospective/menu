79c745b03d99890a07fb1d4a04df9a86cffd8ec7
Mohammed M Rahman <mmrahman14@hotmail.com>
2022-06-21 17:36:22 +0100

    added few refinements and active state to nav

--- components/Navigation/index.js
+++ components/Navigation/index.js
@@ -1,22 +1,24 @@
 import Link from "next/link";
+import { useRouter } from "next/router";
 import styles from "../../styles/Navigation.module.css";
 
 const Navigation = () => {
+    const router = useRouter();
 
     return (
         <>
             <nav className={styles.navigation}>
-                <li>
+                <li className={router.asPath === "/category/mains" ? styles.active : ""}>
                     <Link href="/category/mains">
                         <a>Mains</a>
                     </Link>
                 </li>
-                <li>
+                <li className={router.asPath === "/category/sides" ? styles.active : ""}>
                     <Link href="/category/sides">
                         <a>Sides</a>
                     </Link>
                 </li>
-                <li>
+                <li className={router.asPath === "/category/drinks" ? styles.active : ""}>
                     <Link href="/category/drinks">
                         <a>Drinks</a>
                     </Link>
